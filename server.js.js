f7390b5d9df1204b40060d8b51854605837db857
Damien TANGUY <damientanguy75@gmail.com>
2021-03-08 16:27:04 +0700

    update server.js

--- server.js.js
+++ server.js.js
@@ -10,7 +10,6 @@ const app = express();
 app.use(history());
 app.use(serveStatic(__dirname + '/dist/spa'));
 app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname+'/dist/spa/index.html'));
+    res.sendFile(path.join(__dirname, 'dist/spa', 'index.html'));
 });
-//app.get(/.*/, (req, res) => res.sendFile(path.join(__dirname+'/dist/spa/index.html')));
 app.listen(port);
